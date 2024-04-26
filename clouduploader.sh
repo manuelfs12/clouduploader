367a466fc8421a4c706dd7d8857303f8b2453aba
Manuel Figueroa <manuelfigueroa402@gmail.com>
2024-04-26 17:56:23 -0400

    add check for user arguments

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "Hello World"
+# echo "$BASH_ARGV"
+
+# get arguments
+if [ $# -lt 2 ]
+then
+  echo "Usage:"
+  echo "clouduploader <source_file> <destination>"
+fi
+
+# check if file exist
+# feedback if file note found
+
+# File upload using AWS cli, pass argument 1 as source and last argument as s3 destination
+# aws s3 argument1 argument2
+# upload feedback
