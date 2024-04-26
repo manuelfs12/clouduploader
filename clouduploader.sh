4563aee943ecbb6a8c3f3bb59f182f86b0cbeab9
Manuel Figueroa <manuelfigueroa402@gmail.com>
2024-04-26 18:37:49 -0400

    add aws s3 upload for a single file

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# echo "$BASH_ARGV"
 
 # get arguments
 if [ $# -lt 2 ]
 then
-  echo "Usage:"
-  echo "clouduploader <source_file> <destination>"
+  echo "Usage: clouduploader <source_file> <S3Uri>"
 fi
 
 # check if file exist
-FILE=$1
-test -f "$FILE" && echo "$FILE exist" || echo "$FILE does not exist."
-# feedback if file note found
+test -f "$1" && FILE=$1 && echo "$FILE exist."|| echo "$1 not found."
 
 # File upload using AWS cli, pass argument 1 as source and last argument as s3 destination
+S3URI=$BASH_ARGV
 # aws s3 argument1 argument2
-# upload feedback
+aws s3 cp "$FILE" "$S3URI"
